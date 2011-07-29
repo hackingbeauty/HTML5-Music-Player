78e3a176613154687dd21d9407c54f4d87243f09
eaturspinach <markmuskardin@gmail.com>
2011-07-28 19:19:18 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicPlayer::Application.config.secret_token = '47214660995e07a87a76a01ec201fa6b566284613dbd738bca051036f6d6c67408abcebdafbd9e1fda64157d22b166e6faf115d592605f058a394ad78a992fe3'
